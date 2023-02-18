4e2551cf7b209209a15e5ce4fa4983535879c286
gustavo Xavier <guxfarias7@gmail.com>
2023-02-18 10:51:19 -0300

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,6 @@ app.config.from_object('config') # setando as configs
 db = SQLAlchemy(app) # atribuiçao do banco de dados a variavel db
 
 #import de tabelas diretamente utilizaveis na aplicaçao, para que o arquivo run.py possa reconhecer
-from app.models import tables
+from app.models import tablesUser
 from app.controllers import default
 
